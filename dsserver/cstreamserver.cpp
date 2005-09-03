638b116cab845845bb96766ef383b73863ce3d9f
Martin Runge <martin.runge@web.de>
2005-09-03 09:43:37 +0000

    dedicated class for server added

---
+++ dsserver/cstreamserver.cpp
@@ -0,0 +1,67 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Martin Runge   *
+ *   martin.runge@web.de   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "cstreamserver.h"
+
+CStreamServer::CStreamServer()
+{
+}
+
+
+CStreamServer::~CStreamServer()
+{
+}
+
+
+
+
+/*!
+    \fn CStreamServer::open()
+ */
+int CStreamServer::open()
+{
+    /// @todo implement me
+}
+
+
+/*!
+    \fn CStreamServer::close()
+ */
+void CStreamServer::close()
+{
+    /// @todo implement me
+}
+
+
+/*!
+    \fn CStreamServer::write(char* buffer, int length)
+ */
+int CStreamServer::write(char* buffer, int length)
+{
+    /// @todo implement me
+}
+
+
+/*!
+    \fn CStreamServer::flush()
+ */
+void CStreamServer::flush()
+{
+    /// @todo implement me
+}
---
+++ dsserver/cstreamserver.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Martin Runge   *
+ *   martin.runge@web.de   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef CSTREAMSERVER_H
+#define CSTREAMSERVER_H
+
+/**
+Class provides a server for a stream.
+
+@author Martin Runge
+*/
+class CStreamServer{
+public:
+    CStreamServer();
+
+    ~CStreamServer();
+    int open();
+    void close();
+    int write(char* buffer, int length);
+    void flush();
+
+};
+
+#endif
