4b561753ae55330617f42da6dc70bb56b22e369c
Martin Runge <martin.runge@web.de>
2005-10-31 09:11:38 +0000

    compiles again, not tested yet.

--- dsserver/cstreamserver.cpp
+++ dsserver/cstreamserver.cpp
@@ -19,8 +19,36 @@
  ***************************************************************************/
 #include "cstreamserver.h"
 
-CStreamServer::CStreamServer()
-{
+using namespace std;
+using namespace boost::posix_time;
+
+
+CStreamServer::CStreamServer(int session_id, int transport_buffer_size_in_ms) : 
+            m_first_send_time(not_a_date_time), 
+            m_send_time(not_a_date_time), 
+            m_last_send_time(not_a_date_time) {
+
+  m_payload_size = 1024;
+  m_audio_bytes_per_frame = 2 * sizeof(short);
+
+  m_transport_buffer_size_in_ms = transport_buffer_size_in_ms;
+
+  m_payload_duration_sum = microseconds(0);
+  m_total_play_time = microseconds(0);
+
+  time_duration buffersize = millisec(transport_buffer_size_in_ms);
+
+  m_rtp_packet = new CRTPPacket(m_payload_size);
+
+  m_syncobj.addDuration(buffersize);
+  m_syncobj.frameNr(0);
+  m_syncobj.streamId(0);  
+  m_syncobj.sessionId(session_id);
+  // m_syncobj.serialize();  this is done in open
+
+  m_frames_in_sync_period = 0;
+
+
 }
 
 
@@ -34,9 +62,28 @@ CStreamServer::~CStreamServer()
 /*!
     \fn CStreamServer::open()
  */
-int CStreamServer::open()
+int CStreamServer::open(int audio_bytes_per_second)
 {
-    /// @todo implement me
+  m_last_send_time = microsec_clock::local_time();
+  m_last_payload_duration = not_a_date_time;
+  
+  ptime now = m_last_send_time;
+
+  m_syncobj.frameNr(0);
+  m_syncobj.streamId( m_syncobj.streamId() + 1 );  
+  m_syncobj.serialize();
+
+  m_rtp_packet->payloadType(PAYLOAD_SYNC_OBJ);
+  m_rtp_packet->copyInPayload(m_syncobj.getSerialisationBufferPtr(), m_syncobj.getSerialisationBufferSize());
+
+  m_audio_bytes_per_second = audio_bytes_per_second;
+  
+  m_frames_in_sync_period = 0;
+  m_payload_duration_sum = microseconds(0);
+
+  /// @todo send RTP packet to all clients
+
+  return 0;
 }
 
 
@@ -46,16 +93,80 @@ int CStreamServer::open()
 void CStreamServer::close()
 {
     /// @todo implement me
+    // send end of stream packet
 }
 
 
 /*!
     \fn CStreamServer::write(char* buffer, int length)
  */
-int CStreamServer::write(char* buffer, int length)
-{
-    /// @todo implement me
+int CStreamServer::write(char* buffer, int length) {
+
+    time_duration payload_duration = microseconds((length * 1000000) / m_audio_bytes_per_second);
+
+    m_rtp_packet->copyInPayload(buffer, length);
+    
+
+    m_rtp_packet->seqNum( m_rtp_packet->seqNum() + 1 ); 
+    m_rtp_packet->timestamp( m_frames_in_sync_period );
+    m_rtp_packet->payloadType(PAYLOAD_PCM);
+
+//    measure the quality of the below usleep calculation
+//    now = microsec_clock::local_time();
+//    interval = now - m_last_send_time;
+//    cerr << "interval: " << interval << endl;
+
+
+    list<CSocket*>::iterator iter;
+
+    for(iter = m_socket_list.begin(); iter != m_socket_list.end(); iter++ ) {
+      (*iter)->write(m_rtp_packet->bufferPtr(), m_rtp_packet->usedBufferSize()); 
+    }
+
+    m_frames_in_sync_period += length / m_audio_bytes_per_frame;
+
+//    m_last_send_time = now;    
+
+    if(m_last_payload_duration.is_special() == true) {
+      // if this is the first packet, assume the "last" with same duration 
+      // for sleep calculation below. 
+      
+      cerr << " CStreamSource::readAndSend: ============ STREAM RESET! =============" << endl;
+      m_last_payload_duration = payload_duration;
+      m_time_since_last_packet = m_last_payload_duration;
+
+      m_first_send_time = microsec_clock::universal_time();
+      m_send_time = m_first_send_time;
+      
+      m_payload_duration_sum = payload_duration;
+      // cerr << " payload_duration sum = " << m_payload_duration_sum;
+
+    }
+    else {
+      m_send_time = microsec_clock::universal_time();
+      m_payload_duration_sum += payload_duration;                  
+      // cerr << " payload_duration sum=" << m_payload_duration_sum;
+    }
+
+    m_total_play_time = m_send_time - m_first_send_time;
+    // cerr << " total play time=" << total_play_time;
+
+    time_duration sleep_duration = m_payload_duration_sum - m_total_play_time;
+    long sleep_duration_in_us = sleep_duration.total_microseconds();    
+
+    // cerr << " -> sleeping " << sleep_duration_in_us << " us before sending next packet." << endl;
+    
+    // only sleep, if there is time to sleep. especially on negative values, we are much too late, so don't sleep at all.
+    if(sleep_duration_in_us > 0)
+      usleep(sleep_duration_in_us);
+    else {
+      cerr << " Sleep duration was < 0!" << endl;
+    }
+
+    m_last_payload_duration = payload_duration;
 }
+
+
 
 
 /*!
@@ -65,3 +176,31 @@ void CStreamServer::flush()
 {
     /// @todo implement me
 }
+
+
+list<CSocket*>::iterator CStreamServer::addSocket(CSocket* socket) {
+
+  list<CSocket*>::iterator iter;
+
+  // do some checks on the socket here.
+
+  m_socket_list_mutex.Lock();
+  iter = m_socket_list.insert(m_socket_list.end(), socket);
+  m_socket_list_mutex.UnLock();
+
+}
+
+
+CSocket* CStreamServer::removeSocket(list<CSocket*>::iterator sock_iterator) {
+
+  CSocket* socket;  
+
+  socket = *sock_iterator;
+  
+  m_socket_list_mutex.Lock();
+  m_socket_list.erase(sock_iterator);
+  m_socket_list_mutex.UnLock();
+
+  return socket;
+}
+
--- dsserver/cstreamserver.h
+++ dsserver/cstreamserver.h
@@ -25,15 +25,76 @@ Class provides a server for a stream.
 
 @author Martin Runge
 */
+
+#include <iostream>
+#include <list>
+
+#include <sys/time.h>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+// #include "libsock++.h"
+// #include "libdsaudio.h"
+
+#include "csocket.h"
+#include "csync.h"
+#include "cmutex.h"
+#include "crtppacket.h"
+
+
+
+
 class CStreamServer{
 public:
-    CStreamServer();
+    CStreamServer(int session_id = 1, int transport_buffer_size_in_ms = 1500);
 
     ~CStreamServer();
-    int open();
+
+    int open(int audio_bytes_per_second = 2 * 2 * 44100);
+
     void close();
+
     int write(char* buffer, int length);
+
     void flush();
+
+    std::list<CSocket*>::iterator addSocket(CSocket* socket);
+    CSocket* removeSocket(std::list<CSocket*>::iterator sock_iterator);
+
+private:
+    unsigned long m_num;
+    int m_payload_size;
+
+    int m_num_clients;
+
+    int m_audio_bytes_per_second;
+    int m_audio_bytes_per_frame;
+
+    long m_frames_in_sync_period;
+
+    /** system time when the first packet was sent */
+    boost::posix_time::ptime m_first_send_time;
+
+    /** the actual system time when the last packet was sent */
+    boost::posix_time::ptime m_last_send_time;
+
+    boost::posix_time::ptime m_send_time;
+
+    boost::posix_time::time_duration m_time_since_last_packet;
+    boost::posix_time::time_duration m_payload_duration_sum;
+    boost::posix_time::time_duration m_last_payload_duration;
+    boost::posix_time::time_duration m_total_play_time;
+
+    CRTPPacket *m_rtp_packet;
+    CSync m_syncobj;
+
+    std::list<CSocket*> m_socket_list;
+
+    CMutex m_socket_list_mutex;
+
+    int m_transport_buffer_size_in_ms;
+
+
 
 };
 
