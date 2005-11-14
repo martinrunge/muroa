e7fc20902d0d9f46112b0b680c255bf8a419bde0
Martin Runge <martin.runge@web.de>
2005-11-14 14:14:21 +0000

    introduce a CStreamConnection class

---
+++ dsserver/cstreamconnection.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Martin Runge   *
+ *   martin.runge@web.de   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef CSTREAMCONNECTION_H
+#define CSTREAMCONNECTION_H
+
+#include <csocket.h>
+
+#include <string>
+/**
+@author Martin Runge
+*/
+class CStreamConnection : public CSocket
+{
+public:
+    CStreamConnection(std::string dest_host="", unsigned short dest_port = 0, unsigned short bind_port = 0);
+
+    ~CStreamConnection();
+
+};
+
+#endif
