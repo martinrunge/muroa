68f81e5ecadec2f10cdb54626ffe02455dee5139
Martin Runge <martin.runge@web.de>
2010-10-09 13:17:33 +0000

    Switched to GUI test runner QxRunner

--- muroaGui/muroaserver/unittest/MediaprocessingTestApp.cpp
+++ muroaGui/muroaserver/unittest/MediaprocessingTestApp.cpp
@@ -7,10 +7,11 @@
 
 #include <QTextCodec>
 
+#include <QApplication>
+#include <qxcppunit/testrunner.h>
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
-#include <cppunit/ui/qt/TestRunner.h>
 
 
 int main(int argc, char* argv[])
@@ -18,22 +19,25 @@ int main(int argc, char* argv[])
 
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
 
+    QApplication app(argc, argv);
+
     // Get the top level suite from the registry
     CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
 
     // Adds the test to the list of test to run
-    CppUnit::TextUi::TestRunner runner;
+    //CppUnit::TextUi::TestRunner runner;
+    QxCppUnit::TestRunner runner;
     //CppUnit::QtUi::TestRunner runner;
     runner.addTest( suite );
+    //runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
 
     // Change the default outputter to a compiler error format outputter
-    runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(),
-                                                       std::cerr ) );
+    // runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(), std::cerr ) );
 
     // Run the tests.
-    bool wasSucessful = runner.run();
-    /* bool wasSucessful = true;
-    runner.run(true);*/
+    // bool wasSucessful = runner.run();    // CppUnit::TextUi::TestRunner returns bool here.
+    runner.run();
+    bool wasSucessful = true;
 
     // Return error code 1 if the one of test failed.
     return wasSucessful ? 0 : 1;
