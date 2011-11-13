882666ef56580eb1f170328ec39aa9b349761c3d
Martin Runge <martin.runge@web.de>
2011-11-13 20:31:44 +0100

    missing file

---
+++ libmuroa++/muroaConstants.h
@@ -0,0 +1,14 @@
+/*
+ * muroaConstants.h
+ *
+ *  Created on: 13 Nov 2011
+ *      Author: martin
+ */
+
+#ifndef MUROACONSTANTS_H_
+#define MUROACONSTANTS_H_
+
+#define CREATE_NEW_SESSION  "createNewSession"
+
+
+#endif /* MUROACONSTANTS_H_ */
