d7923b050c9b2470b1a16d0840d51a54d5c849b9
Martin Runge <martin.runge@web.de>
2012-07-17 21:56:02 +0200

    - Added CMake buildsys to legacy projects around muroad and dsserver - Renamed libdsserver to libstreamsrv

---
+++ libstreamsrv/cstreamconnection.cpp
@@ -0,0 +1,114 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Martin Runge   *
+ *   martin.runge@web.de   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "cstreamconnection.h"
+#include "cstreamserver.h"
+
+#include <iostream>
+
+using namespace std;
+
+CStreamConnection::CStreamConnection(CStreamServer* parent, unsigned short bind_port) : m_socket(SOCK_DGRAM, bind_port )
+{
+  m_stream_server = parent;
+  m_socket.setNonBlocking(0);
+  m_socket.recordSenderWithRecv(false);
+}
+
+CStreamConnection::~CStreamConnection()
+{
+
+}
+
+
+
+
+/*!
+    \fn CStreamConnection::connect(CIPv4Address* addr)
+ */
+int CStreamConnection::connect(CIPv4Address* addr)
+{
+    m_client_address = *addr;
+    return m_socket.connect(addr);
+}
+
+
+/*!
+    \fn CStreamConnection::send(char* buffer, int len)
+ */
+int CStreamConnection::send(char* buffer, const int len)
+{
+    // 1) send data away
+    int num = m_socket.write(buffer, len);
+    
+    // 2) check, if there was a rtp packet sent from the client
+    int read_num = m_socket.read(m_rtp_packet.bufferPtr(), m_rtp_packet.bufferSize());   
+
+    // cerr << "m_socket.read ret " << read_num << endl;
+    if(read_num > 0) {
+      handleReceivedPacket();      
+    }
+
+    return num;
+}
+
+
+/*!
+    \fn CStreamConnection::handleReceivedPacket()
+ */
+void CStreamConnection::handleReceivedPacket()
+{
+    cerr << "CStreamConnection::handleReceivedPacket: received a RTP packet from the client." << endl;
+    cerr << "RTP: payloadType = " << m_rtp_packet.payloadType() 
+         << " sessionID = " << m_rtp_packet.sessionID() 
+         << " streamID = " << m_rtp_packet.streamID() << endl; 
+
+    if(m_rtp_packet.payloadType() == PAYLOAD_SYNC_OBJ) {
+      CSync tmp_sync(&m_rtp_packet);
+      tmp_sync.deserialize();
+
+      if(tmp_sync.syncType() == SYNC_REQ_STREAM) {
+        // the clients needs a sync object for this stream !!!
+        CSync* session_sync_obj;
+        session_sync_obj = m_stream_server->getSyncObj(tmp_sync.sessionId(), tmp_sync.streamId());
+        if(session_sync_obj != 0) {
+
+          CRTPPacket packet(session_sync_obj->sessionId(), session_sync_obj->streamId(), sizeof(CSync), true);
+
+          session_sync_obj->serialize();
+          packet.copyInPayload(session_sync_obj->getSerialisationBufferPtr(), session_sync_obj->getSerialisationBufferSize());
+      
+          packet.payloadType(PAYLOAD_SYNC_OBJ);
+
+          m_socket.write(packet.bufferPtr(), packet.usedBufferSize());        
+        }
+      }  
+
+    } 
+
+}
+
+
+/*!
+    \fn CStreamConnection::method_1()
+ */
+CIPv4Address* CStreamConnection::getClientAddress()
+{
+    return &m_client_address;
+}
---
+++ libstreamsrv/cstreamserver.h
@@ -0,0 +1,126 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Martin Runge   *
+ *   martin.runge@web.de   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef CSTREAMSERVER_H
+#define CSTREAMSERVER_H
+
+/**
+Class provides a server for a stream.
+
+@author Martin Runge
+*/
+
+#include <iostream>
+#include <list>
+
+#include <sys/time.h>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+// #include "libsock++.h"
+// #include "libdsaudio.h"
+
+#include "cstreamconnection.h"
+#include "csync.h"
+#include "cmutex.h"
+#include "crtppacket.h"
+
+
+
+
+class CStreamServer{
+public:
+    CStreamServer(int session_id = 1, int transport_buffer_size_in_ms = 1500);
+
+    ~CStreamServer();
+
+    int open(int audio_bytes_per_second = 2 * 2 * 44100);
+
+    void close();
+
+    int write(char* buffer, int length);
+
+    void flush();
+
+    std::list<CStreamConnection*>::iterator addStreamConnection(CStreamConnection* conn);
+    CStreamConnection* removeStreamConnection(std::list<CStreamConnection*>::iterator conn_iterator);
+
+    void removeClient(CIPv4Address* addr);
+    void removeClient(std::list<CStreamConnection*>::iterator iter);
+    std::list<CStreamConnection*>::iterator addClient(CIPv4Address* addr);
+    CSync* getSyncObj(uint32_t session_id, uint32_t stream_id);
+
+    /*!
+      \fn CStreamServer::adjustClientListTo(std::vector<std::string> clients)
+      \brief  specify a new list of clients and adjust the used list to that new list.
+
+      This is done in two steps: first, search the connection list for cliet connections not listed any more in the new list and remove them. Second, search the new list for clients, that are not yet listed in the modified connection list and add them.
+    */
+    void adjustClientListTo(std::list<std::string> clients);
+
+    void stdClientPort(int port);
+    int stdClientPort(void);
+    void listClients(void);
+
+private:
+    unsigned long m_num;
+    int m_payload_size;
+
+    int m_num_clients;
+
+    int m_audio_bytes_per_second;
+    int m_audio_bytes_per_frame;
+
+    long m_frames_in_sync_period;
+
+    /** system time when the first packet was sent */
+    boost::posix_time::ptime m_first_send_time;
+
+    /** the actual system time when the last packet was sent */
+    boost::posix_time::ptime m_last_send_time;
+
+    boost::posix_time::ptime m_send_time;
+
+    boost::posix_time::time_duration m_time_since_last_packet;
+    boost::posix_time::time_duration m_payload_duration_sum;
+    boost::posix_time::time_duration m_last_payload_duration;
+    boost::posix_time::time_duration m_total_play_time;
+
+    boost::posix_time::time_duration m_transport_buffer_duration;
+  
+    CRTPPacket *m_rtp_packet;
+    CSync m_syncobj;
+
+    std::list<CStreamConnection*> m_connection_list;
+
+    CMutex m_connection_list_mutex;
+
+    unsigned long m_session_id;
+
+    unsigned long m_stream_id;
+
+    int m_std_client_port;
+
+
+
+private:
+    void sendToAllClients(CRTPPacket* packet);
+};
+
+#endif
