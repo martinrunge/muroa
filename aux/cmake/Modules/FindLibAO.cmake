6ae47c362fadfbfe3fd9283e6fdf9428686c47f0
martinrunge <martin.runge@web.de>
2013-11-17 11:01:12 +0100

    ... more work to to improve build system ...

--- aux/cmake/Modules/FindLibAO.cmake
+++ aux/cmake/Modules/FindLibAO.cmake
@@ -4,16 +4,17 @@ MESSAGE(STATUS "Looking for LibAO")
 FIND_PATH(LibAO_INCLUDE_DIR ao.h /usr/include/ao /usr/local/include/ao)
 FIND_LIBRARY(LibAO_LIBRARY NAMES ao PATH /usr/lib /usr/local/lib) 
 
+set(LibAO_LIBRARIES ${LibAO_LIBRARY})
+set(LibAO_INCLUDE_DIRS ${LibAO_INCLUDE_DIR})
 
 # handle the QUIETLY and REQUIRED arguments and set LIBAO_FOUND to TRUE
 # if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LibAO  DEFAULT_MSG
-                                  LibAO_LIBRARY LibAO_INCLUDE_DIR)
+                                  LibAO_LIBRARIES LibAO_INCLUDE_DIRS)
 
 IF (LIBAO_FOUND)
-   set(LibAO_LIBRARIES ${LibAO_LIBRARY})
-   set(LibAO_INCLUDE_DIRS ${LibAO_INCLUDE_DIR})
+   message(STATUS "Found libAO: ${LibAO_LIBRARY}")
    
    MARK_AS_ADVANCED(LibAO_LIBRARIES)
    MARK_AS_ADVANCED(LibAO_INCLUDE_DIRS)
