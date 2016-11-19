c9316663bc0cfa5c0966909053a3abea1184f75f
martinrunge <martin.runge@web.de>
2016-11-19 16:34:58 +0100

    improved packaging:
    build three debian packages, muroa-common, muroad and wssrv

--- aux/scripts/deploy2dpkg.sh
+++ aux/scripts/deploy2dpkg.sh
@@ -36,14 +36,33 @@ ssh pi@raspi-deb-packer "pwd && git/muroa/aux/scripts/build-deb-pkg.sh"
 # call thos on targets to add signing key:
 # sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5FBF5DA1
 #
+pushd $DEBDIR/..
+
+dpkg_list=()
+while IFS= read -d $'\0' -r file ; do
+    dpkg_list=("${dpkg_list[@]}" "$file")
+done < <(find -name "*.deb" -print0)
+
+# echo "DIE Liste:   ${dpkg_list[@]}"
+
+for file in "${dpkg_list[@]}" ; do
+    echo "package was built: '$file' -> adding it to repository in '$REPODIR'"
+    reprepro -b $REPODIR includedeb jessie $file
+done
+
+popd
+
+#FN=$(find $DEBDIR/.. -name "*.deb" -type f)
+#if [ $? -ne 0 ]; then
+#    echo "Error: no debian package was found inside $DEBDIR/.."
+#    echo "Maybe the build on the RaspberryPi was no successful or the directory is not mounted the expected way"
+#else
+#    echo "package was built: '$FN' -> adding it to repository in '$REPODIR'"
+#    reprepro -b $REPODIR includedeb jessie $FN
+#fi
 
 
-FN=$(find $DEBDIR/.. -name "muroad*.deb" -type f)
-if [ $? -ne 0 ]; then
-    echo "Error: no debian package was found inside $DEBDIR/.."
-    echo "Maybe the build on the RaspberryPi was no successful or the directory is not mounted the expected way"
-else
-    echo "package was built: '$FN' -> adding it to repository in '$REPODIR'"
-    reprepro -b $REPODIR includedeb jessie $FN
-fi
+# mirror the repo to deb.muroa.org
+cd $DEBDIR
+rsync -avze ssh .  u71829316@muroa.org:~/deb/raspbian/
 
