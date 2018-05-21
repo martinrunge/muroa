b94ca270aa3b80c3d579f989a8fa2aafca932b0f
Martin Runge <martin.runge@web.de>
2018-05-21 18:21:05 +0200

    change path to cross compiler according to wiki article

--- aux/cmake/arm-raspi-stretch.cmake
+++ aux/cmake/arm-raspi-stretch.cmake
@@ -11,18 +11,18 @@ SET(CMAKE_SYSTEM_VERSION 1)
 set(ARCH "arm")
 set(HOME  $ENV{HOME})
 set(CMAKE_LIBRARY_ARCHITECTURE "arm-linux-gnueabihf" )
-set(SYSROOT  ${HOME}/x-tools-new/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot)
+set(SYSROOT  ${HOME}/x-tools-stretch/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   ${HOME}/x-tools-new/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER ${HOME}/x-tools-new/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-g++)
+SET(CMAKE_C_COMPILER   ${HOME}/x-tools-stretch/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER ${HOME}/x-tools-stretch/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-g++)
 
 # set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-)
 
 # where is the target environment
-SET(CMAKE_FIND_ROOT_PATH  ${HOME}/x-tools-new/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot)
+SET(CMAKE_FIND_ROOT_PATH  ${HOME}/x-tools-stretch/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot)
 # where is the target environment
-SET(SYSROOT_PATH  ${HOME}/x-tools-new/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot)
+SET(SYSROOT_PATH  ${HOME}/x-tools-stretch/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot)
 # SET(CMAKE_C_FLAGS   "-mfloat-abi=hard" CACHE STRING "-mfloat-abi=hard")
 # SET(CMAKE_CXX_FLAGS  "-mfloat-abi=hard"  CACHE STRING "-mfloat-abi=hard")
 # SET(LINK_FLAGS  "" CACHE STRING "--sysroot=${SYSROOT_PATH}")
